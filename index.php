a38ea85cff6c159c3c6baa3c3324c6378a3a5087
Gordana Minovska <gminovska@users.noreply.github.com>
2017-01-13 21:12:41 -0800

    Merge with the master  (#1)
    * Delete groupProject1.php
    
    * fixing file structure...
    
    * fixing file structure...
    
    * fixing file structure...
    
    * Delete Kelvin.php
    
    * Delete index.php
    
    * Delete style.css
    
    * file structure, validation updated
    
    Adopted Shad's file structure, changed doc.php -> formhandler.php, and added validation of user input.
    
    * Update formhandle.php

--- index.php
+++ index.php
@@ -1,83 +1,40 @@
 <?php
-/**
- * This is my testing document, to make sure the functions work.
- * I created the objects to calculate the conversions
- * Created by PhpStorm.
- * User: Ekogoca
- * Date: 01/10/2017
- * Time: 11:14 PM
- */
-require 'Conversion.php';
-require 'Celsius.php';
-require 'Fahrenheit.php';
-require 'Kelvin.php';
-//@todo validate user input
 
-//store user input in variables
-if($_POST){
-//    echo var_dump($_POST);
-    $temperature = $_POST['temperatureValue'];
-    $fromUnit = $_POST['fromUnit'];
-    $toUnit = $_POST['toUnit'];
-}else {
-    $temperature = 0;
-    $fromUnit='';
-    $toUnit = '';
-    $conversionValue_formatted = '';
-}
-
-//create the appropriate object
-switch ($fromUnit){
-    case 'celsius':
-        $fromCelsius = new Celsius($temperature);
-        $conversionValue = $fromCelsius->Convert($toUnit);
-//        echo $conversionValue;
-        break;
-    case 'fahrenheit':
-        $fromFahrenheit = new Fahrenheit($temperature);
-        $conversionValue = $fromFahrenheit->Convert($toUnit);
-//        echo $conversionValue;
-        break;
-    case 'kelvin':
-        $fromKelvin = new Kelvin ($temperature);
-        $conversionValue = $fromKelvin->Convert($toUnit);
-//        echo $conversionValue;
-        break;
-}
-
-//display the result to the user
-if (isset($conversionValue))
-$conversionValue_formatted = $temperature . ' ' . $fromUnit . ' = ' . $conversionValue .' '. $toUnit;
-
+require 'includes/Conversion.php';
+require 'includes/Celsius.php';
+require 'includes/Fahrenheit.php';
+require 'includes/Kelvin.php';
+require 'includes/formhandle.php';
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Temperature Converter</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <div id="wrapper">
 <h1>Temperature Converter</h1>
-    <div id="theForm">
-<form method="post" action="index.php">
+  
+<form id="theForm" method="post" action="index.php">
     <input type="text" name="temperatureValue">
     <select name="fromUnit">
-        <option value="celsius">Celsius</option>
-        <option value="fahrenheit">Fahrenheit</option>
-        <option value="kelvin">Kelvin</option>
+        <option value="celsius">&#x2103;elsius</option>
+        <option value="fahrenheit">&#8457;ahrenheit</option>
+        <option value="kelvin">&#8490;elvin</option>
     </select>
     <span>To</span>
     <select name="toUnit">
-        <option value="celsius">Celsius</option>
-        <option value="fahrenheit">Fahrenheit</option>
-        <option value="kelvin">Kelvin</option>
+        <option value="celsius">&#x2103;elsius</option>
+        <option value="fahrenheit">&#8457;ahrenheit</option>
+        <option value="kelvin">&#8490;elvin</option>
     </select>
     <input type="submit" value="Convert">
-    </div>
-    <p><?php echo $conversionValue_formatted ?></p>
-    <p>A placeholder for error message</p>
+  <br/> <br/>
+    <h3><i><?php echo $conversionValue_formatted ?></i></h3>
+    <h4 class="error"><?php echo htmlspecialchars($error_message); ?></h4>
 
 
 </form>
